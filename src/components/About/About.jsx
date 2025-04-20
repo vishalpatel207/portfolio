266d1a5911b608f789971eeb16adfb246f016cfd
rahul-ninave <rahulkumar.ninave@provabmail.com>
2025-04-20 15:25:02 +0530

    work on the feedback changes

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -36,28 +36,27 @@ const About = () => {
           </h3>
           {/* About Me Paragraph */}
           <p className="text-base sm:text-lg md:text-lg text-gray-400 mb-10 mt-8 leading-relaxed">
-          I  am a Frontend Developer with over 5 months of experience in building
-            responsive and scalable web applications. I specialize in crafting
-            intuitive user interfaces using HTML, CSS, JavaScript, and modern
-            frontend libraries. My focus is on creating seamless user
+            I am a Frontend Developer with over 5 months of experience in
+            building responsive and scalable web applications. I specialize in
+            crafting intuitive user interfaces using HTML, CSS, JavaScript, and
+            modern frontend libraries. My focus is on creating seamless user
             experiences with clean, efficient code and responsive design
             principles. I'm passionate about bringing designs to life and
             continuously improving the usability and performance of web
             applications.
           </p>
           {/* Resume Button */}
-        <a
-  href="/UPGRADE2-1.pdf"
-  target="_blank"
-  rel="noopener noreferrer"
-  className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
-  style={{
-    background: "linear-gradient(90deg, #8245ec, #a855f7)",
-    boxShadow: "0 0 2px #8245ec, 0 0 2px #8245ec, 0 0 40px #8245ec",
-  }}
->
-DOWNLOAD CV</a>
-
+          <a href="/UPGRADE2-1.pdf" 
+            target="_blank"
+            rel="noopener noreferrer"
+            className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
+            style={{
+              background: "linear-gradient(90deg, #8245ec, #a855f7)",
+              boxShadow: "0 0 2px #8245ec, 0 0 2px #8245ec, 0 0 40px #8245ec",
+            }}
+          >
+            DOWNLOAD CV
+          </a>
         </div>
         {/* Right Side */}
         <div className="md:w-1/2 flex justify-center md:justify-end">
